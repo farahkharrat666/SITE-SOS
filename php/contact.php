df71a1ca7b2cf6d5c705d2953626945b29e18389
Ellouze Mohamed <47985183+MohamedEllouze@users.noreply.github.com>
2021-07-08 10:44:29 +0200

    SOS

--- php/contact.php
+++ php/contact.php
@@ -1,9 +1,9 @@
+
 <?php
-// Email Submit
-// Note: filter_var() requires PHP >= 5.2.0
+
 if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
  
-  // detect & prevent header injections
+
   $test = "/(content-type|bcc:|cc:|to:)/i";
   foreach ( $_POST as $key => $val ) {
     if ( preg_match( $test, $val ) ) {
@@ -15,10 +15,8 @@ $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'Reply-To: ' . $_POST["email"] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-  //
-  mail( "khaitawng2014@gmail.com", $_POST['subject'], $_POST['message'], $headers );
  
-  //      ^
-  //  Replace with your email 
+  mail( "farahkharrat666@gmail.com", $_POST['subject'], $_POST['message'], $headers );
+
 }
 ?>
